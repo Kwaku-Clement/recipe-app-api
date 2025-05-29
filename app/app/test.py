fa92f5757084248d82258504ff5b11e7db2d6063
kwaku-clement <aduclem@gmail.com>
2025-05-29 18:27:55 +0000

    fix: minor fix

--- app/app/test.py
+++ app/app/test.py
@@ -1,19 +1,18 @@
 """
-sample test file
+Sample test file.
 """
-
 
 from django.test import SimpleTestCase
 from app import calc
+
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-
 
     def test_add_numbers(self):
         """Test adding two numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test subtracting two numbers."""
